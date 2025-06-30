41e923205717ad3dcd5a34b2113bc7fe8b56d42b
Michael <michaelv.pavkovic@gmail.com>
2025-06-30 22:44:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# DB2 rootless Docker image
+## Intro
+The official image for DB2 requires `--privileged`. This one is rootless, and does not require `--privileged`. Built on and working with Podman, and DB2 v11.5.9. Docker I haven't actually tested but there's nothing that should cause issues. I also have not tested other versions of DB2
+
+**Do NOT use this in production.** I created this for testing purposes only. It doesn't even have persistence, and I have no plans to implement it.
+
+Hope someone finds this useful
+
+## Credits
+I did not create the entrypoint script, I took the one from [this project](https://github.com/Taskana/taskana/blob/master/docker-databases/db2_11-5/entrypoint.sh) - I modified it a little to work with my image. See the entrypoint.sh file itself for more information.
+
+## How to build
+### Step 1
+Download a tar archived installer for DB2 from IBM. I will not be providing this, you need to bring your own, and then edit the Dockerfile accordingly. I recommend getting version v11.5.9. Verify the name matches the one in the Dockerfile
+
+### Step 2
+Pick the base you want (currently only Rocky Linux 9), change to the directory accordingly. Move the DB2 installer herre. 
+
+Run:
+```
+docker build --tag localhost/db2-rootless:latest .
+```
+
+(Optional) After build is finished, clear the dangling images left over from the build. I recommend doing this, as the images are in the gigabytes
+```
+docker image prune 
+``` 
+
+### Step 3
+```
+docker run -d --name mydb2 -p 50000:50000 -e LICENSE=accept localhost/db2-rootless:latest 
+```
+
+This creates a fully barebones install, you're gonna have to make a db and anything else by yourself.
+You can test if it's working by running this in the host system:
+```
+docker exec -it mydb2 /bin/bash
+```
+And in the container, run:
+```
+db2 create database
+db2 list db directory
+```
+
+### Step 4
+Enjoy (not in production please)
---
+++ rocky-linux-9/entrypoint/entrypoint.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#
+#   Initialize DB2 instance in a Docker container
+#
+# # Authors:
+#   * Leo (Zhong Yu) Wu       <leow@ca.ibm.com>
+#   * Boris Manojlovic        <boris@steki.net>
+#
+# Copyright 2015, IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###########################################################################
+# Modified by Chipovaneyyy on Github. I am not affiliated with anyone mentioned above.
+#
+# I have removed switching the user from the start/stop/restart functions, 
+# removed the password change, and modified the way license accepting is handled.
+
+pid=0
+
+source "${DB2_INSTALL_LOC}/db2profile"
+
+function log_info {
+ echo -e $(date '+%Y-%m-%d %T')"\e[1;32m $@\e[0m"
+}
+
+function log_error {
+ echo -e >&2 $(date +"%Y-%m-%d %T")"\e[1;31m $@\e[0m"
+}
+
+function stop_db2 {
+  log_info "stopping database engine"
+  db2stop force
+}
+
+function start_db2 {
+  log_info "starting database engine"
+  db2start
+}
+
+function restart_db2 {
+  # if you just need to restart db2 and not to kill this container
+  # use docker kill -s USR1 <container name>
+  kill ${spid}
+  log_info "Asked for instance restart doing it..."
+  stop_db2
+  start_db2
+  log_info "database instance restarted on request"
+}
+
+function terminate_db2 {
+  kill ${spid}
+  stop_db2
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  log_info "database engine stopped"
+  exit 0 # finally exit main handler script
+}
+
+trap "terminate_db2"  SIGTERM
+trap "restart_db2"   SIGUSR1
+
+if [ "${LICENSE}" != "accept" ];then
+    log_error "error: LICENSE not set to 'accept'"
+    log_error "Please set '-e LICENSE=accept' to accept License before use the DB2 software contained in this image."
+    exit 1
+fi
+
+if [[ $1 = "start" ]]; then
+  # Create db2nodes.cfg
+  # This is necessary because DB2 was installed in a separate stage, with a different hostname
+  echo "0 ${HOSTNAME} 0" > ${DB2_INSTALL_LOC}/db2nodes.cfg
+
+  log_info "Initializing container"
+  start_db2
+  log_info "Database db2diag log following"
+  tail -f ${DB2_INSTALL_LOC}/db2dump/DIAG0000/db2diag.log &
+  export pid=${!}
+  while true
+  do
+    sleep 10000 &
+    export spid=${!}
+    wait $spid
+  done
+else
+  exec "$1"
+fi
